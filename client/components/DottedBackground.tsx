6f9b578cc54d4e051233678be1fa8ea68abe154d
Nauval Rizky <enuma.alrizky@gmail.com>
2025-12-18 05:31:48 +0700

    fixup dotted background covering scrollbar

--- client/components/DottedBackground.tsx
+++ client/components/DottedBackground.tsx
@@ -1,5 +1,5 @@
 export function DottedBackground() {
   return (
-    <div className="absolute inset-0 h-full w-full bg-[radial-gradient(rgba(255,255,255,0.15)_1px,transparent_1px)] bg-size-[24px_24px]"></div>
+    <div className="absolute inset-0 -z-1 h-full w-full bg-[radial-gradient(rgba(255,255,255,0.15)_1px,transparent_1px)] bg-size-[24px_24px]"></div>
   );
 }
