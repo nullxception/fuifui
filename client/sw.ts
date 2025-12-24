d1815b4651dfc0f53b2ca634f04f78d7541fcad3
Nauval Rizky <enuma.alrizky@gmail.com>
2025-12-24 23:22:50 +0700

    feat: Add network timeout and response validation plugin to ServiceWorker

--- client/sw.ts
+++ client/sw.ts
@@ -1,5 +1,5 @@
 /// <reference lib="webworker" />
-import { clientsClaim } from "workbox-core";
+import { clientsClaim, type WorkboxPlugin } from "workbox-core";
 import { registerRoute } from "workbox-routing";
 import {
   CacheFirst,
@@ -13,29 +13,49 @@ declare const self: ServiceWorkerGlobalScope;
 clientsClaim();
 self.skipWaiting();
 
+const ensureNetworkOk: WorkboxPlugin = {
+  fetchDidSucceed: async ({ response }) => {
+    if (!response || !response.ok) {
+      throw new Error("Bad response");
+    }
+    return response;
+  },
+};
+
 // Bun HMR bypass
 registerRoute(({ url }) => url.pathname.startsWith("/_bun"), new NetworkOnly());
 
 // Cacheable RPC calls
 registerRoute(
   ({ url }) => /^\/rpc\/(info\.|images\.by)/.test(url.pathname),
-  new NetworkFirst({ cacheName: "rpc" }),
+  new NetworkFirst({
+    cacheName: "rpc",
+    networkTimeoutSeconds: 3,
+    plugins: [ensureNetworkOk],
+  }),
 );
 
 // Bundled CSS and JS
 registerRoute(
   ({ request }) => ["script", "style"].includes(request.destination),
-  new StaleWhileRevalidate({ cacheName: "static-resources" }),
+  new StaleWhileRevalidate({
+    cacheName: "static-resources",
+    plugins: [ensureNetworkOk],
+  }),
 );
 
 // Images (Icons, user uploads, output images) and fonts
 registerRoute(
   ({ request }) => ["image", "font"].includes(request.destination),
-  new CacheFirst({ cacheName: "assets" }),
+  new CacheFirst({ cacheName: "assets", plugins: [ensureNetworkOk] }),
 );
 
 // SPA routing
 registerRoute(
   ({ request }) => request.mode === "navigate",
-  new NetworkFirst({ cacheName: "pages" }),
+  new NetworkFirst({
+    cacheName: "pages",
+    networkTimeoutSeconds: 3,
+    plugins: [ensureNetworkOk],
+  }),
 );
